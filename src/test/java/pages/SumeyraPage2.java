313c436d2c6fd497a1a144f5d46e54242ce40bc1
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-09-17 16:01:33 +0200

    v

--- src/test/java/pages/SumeyraPage2.java
+++ src/test/java/pages/SumeyraPage2.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class SumeyraPage2 {
-
-
     public SumeyraPage2(){
         PageFactory.initElements(Driver.getDriver(),this);}
 
