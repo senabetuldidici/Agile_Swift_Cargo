b280cd5792b676f870d0ca86a9c0298d80fdd70b
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-09-20 01:52:22 +0200

    sum

---
+++ src/test/java/stepdefinitions/ScenarioStepDefinition.java
@@ -0,0 +1,9 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+
+public class ScenarioStepDefinition {
+    @And("Sidebar menüden Ticket baslıgına tıklar")
+    public void sidebarMenüdenTicketBaslıgınaTıklar() {
+    }
+}
